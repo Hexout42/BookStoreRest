de77fc29ba68913a725772664afe8ac54bd60ffc
Hexout42 <baskakov.igo@yandex.ru>
2023-02-07 14:54:37 +0300

    webflux в контроллере BookController

--- src/main/java/ru/lernup/bookstore/controller/BookController.java
+++ src/main/java/ru/lernup/bookstore/controller/BookController.java
@@ -1,7 +1,10 @@
 package ru.lernup.bookstore.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.lernup.bookstore.service.ControllerService;
 import ru.lernup.bookstore.view.BookView;
 
@@ -17,17 +20,19 @@ public class BookController {
         this.controllerService = controllerService;
     }
     @GetMapping
-    public List<BookView> getAllBook(){
-        return controllerService.getAllBook();
+    public Flux<BookView> getAllBook(){
+        return
+                Flux.fromStream(controllerService.getAllBook().stream());
     }
     @GetMapping("/{id}")
-    public  BookView getBookById(@PathVariable("id") Long id){
-        return controllerService.getBookById(id);
+    public Mono<BookView> getBookById(@PathVariable("id") Long id){
+
+        return Mono.just(controllerService.getBookById(id));
     }
     @PreAuthorize("hasRole(\"ROLE_ADMIN\")")
     @PostMapping
-    public BookView addBook(@RequestBody BookView bookView){
-        return controllerService.addBook(bookView);
+    public Mono<BookView> addBook(@RequestBody BookView bookView){
+        return Mono.just(controllerService.addBook(bookView));
     }
     @PreAuthorize("hasRole(\"ROLE_ADMIN\")")
     @DeleteMapping("/{id}")
